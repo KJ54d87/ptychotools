08915b05eba094c02a8903f31168ec13ee1d1f59
Kang Thinkpad <kangkuan9@gmail.com>
2026-01-07 14:02:33 -0800

    Generate circle mask is done

--- contrast/contrast.py
+++ contrast/contrast.py
@@ -1,16 +1,30 @@
-from PIL import Image, ImageDraw
+import io
+from PIL import Image, ImageDraw, ImageColor
 import numpy as np
 import matplotlib.pyplot as plt
-from generate_mesh import generate_mesh
+from generate_mesh import generate_mesh, generate_circle_mesh
+from calculate_intensities import intensity_contrast, luminence_contrast, michelsons_contrast
 
 IMG = "obj_phase_roi_Niter120.tiff"
 frames = 24
+contrast_function = "Michelsons" #Options are(will be) Michelsons, RMS, max_intensity
 
 with Image.open(IMG) as sample:
     #print(sample.__dir__())
-    mesh = generate_mesh(4, 290, 293, 13.7, 2.9, -6, 20, 13.7, 111.7, -100, 100)
+    #newImg = Image.frombytes("rgb", (480, 480))
+    mesh = generate_mesh(4, 290, 293, 13.7, 2.9, -6, 12, 13.7, 111.7, -15, 5)
     draw = ImageDraw.Draw(sample)
     for pt in mesh:
         draw.circle(pt, 6, width=1 , outline="blue")
         
+    mesh = generate_circle_mesh(290, 293, 4)
+    
+    #so apparently I can only draw one thing at a time. 
+    #This program is curse
+    #bruh
+    
+        
+    
+    sample.save("test.png")
+    
         
--- contrast/generate_mesh.py
+++ contrast/generate_mesh.py
@@ -3,17 +3,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def generate_mesh(size: int, x: float, y: float, V1_length: float, V1_degree: float, V1_min: int, V1_max: int, V2
+def generate_mesh(size: int, x: float, y: float, V1_length: float, V1_degree: float, V1_min: int, V1_max: int, V2_length: float, V2_degree: float, V2_min: int, V2_max: int):
 
 
     V1_angle = radians(V1_degree)
     V2_angle = radians(V2_degree)
 
-    mesh_basis = np.array([[V1_length*cos(V1_angle), V1_length*sin(V1_angle)], [V2_length*cos(V2_angle), V2_lengt
+    mesh_basis = np.array([[V1_length*cos(V1_angle), V1_length*sin(V1_angle)], [V2_length*cos(V2_angle), V2_length*sin(V2_angle)]])
 
     POINT1 = np.array([x,y], dtype = float)
 
-    big_mesh = np.array([POINT1+mesh_basis[0]*i+mesh_basis[1]*j for i in range(V1_min, V1_max) for j in range(V2_
+    big_mesh = np.array([POINT1+mesh_basis[0]*i+mesh_basis[1]*j for i in range(V1_min, V1_max) for j in range(V2_min, V2_max)])
     
     return big_mesh
 
+def calc_dist(pt: [int]):
+    return pt[0]**2 + pt[1]**2
+
+def generate_circle_mesh(x, y, r):
+    #make a circle at origin and translate later
+    square = np.array([[x,y] for x in range(-r, r) for y in range(-r, r)])
+    
+    #print(square.shape)
+    calc_dist_vectorized = np.vectorize(calc_dist, signature = "(2) -> ()")
+    dist = calc_dist_vectorized(square)
+    
+    circle_mask = dist < r**2
+    
+    circle = square[circle_mask]
+    
+    circle = circle + [x, y]
+    return circle
