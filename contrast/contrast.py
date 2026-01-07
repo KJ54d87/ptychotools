60e2e1e911198607d6851ca472ed89ad2e0c74bd
Kang Thinkpad <kangkuan9@gmail.com>
2026-01-07 14:08:11 -0800

    cause

--- contrast/contrast.py
+++ contrast/contrast.py
@@ -12,12 +12,14 @@ contrast_function = "Michelsons" #Options are(will be) Michelsons, RMS, max_inte
 with Image.open(IMG) as sample:
     #print(sample.__dir__())
     #newImg = Image.frombytes("rgb", (480, 480))
-    mesh = generate_mesh(4, 290, 293, 13.7, 2.9, -6, 12, 13.7, 111.7, -15, 5)
+    #mesh = generate_mesh(4, 290, 293, 13.7, 2.9, -6, 12, 13.7, 111.7, -15, 5)
     draw = ImageDraw.Draw(sample)
-    for pt in mesh:
-        draw.circle(pt, 6, width=1 , outline="blue")
-        
+    #for pt in mesh:
+        #draw.circle(pt, 6, width=1 , outline="blue")
+    
     mesh = generate_circle_mesh(290, 293, 4)
+    
+    draw.point(mesh, fill = "red")
     
     #so apparently I can only draw one thing at a time. 
     #This program is curse
