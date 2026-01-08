1364eeccab3818edbbd78419c50a587a028d4ff5
Kang Thinkpad <kangkuan9@gmail.com>
2026-01-07 22:27:14 -0800

    working minimum product

--- contrast/contrast.py
+++ contrast/contrast.py
@@ -8,68 +8,68 @@ from calculate_intensities import intensity_contrast, luminence_contrast, michel
 
 IMG = "obj_phase_roi_Niter120.tiff"
 frames = 24
-contrast_function = intensity_contrast  #Options are(will be) Michelsons, RMS, max_intensity
-size = 480 
+contrast_function = RMS #Options are(will be) Michelsons, RMS, max_intensity
+size = 480
 
 with Image.open(IMG) as sample:
     sample = sample.convert("rgb") #convert to rgb so we can draw
     sample = Image.fromarray(np.array(sample)) # wipe meta data so we can draw multiple colors
     #sample = sample.convert("rgb")
-    for i in range()
-    mesh = generate_mesh(4, 290, 293, 13.7, 2.9, 0, 1, 13.7, 111.7, -15, 5)
     draw = ImageDraw.Draw(sample)
-    #for pt in mesh:
-    #    draw.circle(pt, 6, width=1 , outline="blue")
-    
-    #for i in range(len(mesh)):
-    #    print(mesh[i])
-    
-    #generare numpy list
-    circles = [generate_circle_mesh(mesh[i][0], mesh[i][1], 5) for i in range(len(mesh))] 
-    
-    circles_data = []
-    for circle in circles:
-        #draw.point(circle.tolist(), fill = "red")
-        circle_data = []
-        for pxl in circle:
-            if pxl[0] >=0 and pxl[0]<size and pxl[1]>=0 and pxl[1]<size:
-                circle_data.append(sample.getpixel(pxl))
-        circles_data.append(circle_data)
-    #print(circles_data)
-    
-    intensity = []
-    min_val = 9999999
-    max_val = 0
-    for circle_data in circles_data:
-        #print(contrast_function(circle_data))
-        intensity.append(float(contrast_function(circle_data)))
-        if intensity[-1] > max_val:
-            max_val = intensity[-1]
-        if intensity[-1] < min_val:
-            min_val = intensity[-1]
-    
-    color_map = mpl.cm.bwr
-    norm = mpl.colors.Normalize(vmin=min_val, vmax=max_val)
+    for col in range(-6 ,10):
+        mesh = generate_mesh(4, 290, 293, 13.7, 2.9, col, col+1, 13.7, 111.7, -10, 6)
+        #for pt in mesh:
+        #    draw.circle(pt, 6, width=1 , outline="blue")
 
-    color = []
-    for i in range(len(intensity)):
-        rgb = color_map(norm(intensity[i]), bytes = True)
-        color.append(rgb[:3])
-    
-    #print(color)
-    
-    #print(circles[0])
-    #print(color[0])
-    
-    for i in range(len(intensity)):
-        draw.point(circles[i].tolist(), fill=color[i])
-    
-    #print(intensity)
-    #color_layer.save("color.png")
-    #so apparently I can only draw one thing at a time. 
-    #This library is cursed
-    #bruh    
-    
-    sample.save("test.png")
-    
-        
+        #for i in range(len(mesh)):
+        #    print(mesh[i])
+
+        #generare numpy list
+        circles = [generate_circle_mesh(mesh[i][0], mesh[i][1], 7) for i in range(len(mesh))]
+
+        circles_data = []
+        for circle in circles:
+            #draw.point(circle.tolist(), fill = "red")
+            circle_data = []
+            for pxl in circle:
+                if pxl[0] >=0 and pxl[0]<size and pxl[1]>=0 and pxl[1]<size:
+                    circle_data.append(sample.getpixel(pxl))
+            circles_data.append(circle_data)
+        #print(circles_data)
+
+        intensity = []
+        min_val = 9999999
+        max_val = 0
+        for circle_data in circles_data:
+            #print(contrast_function(circle_data))
+            intensity.append(float(contrast_function(circle_data)))
+            if intensity[-1] > max_val:
+                max_val = intensity[-1]
+            if intensity[-1] < min_val:
+                min_val = intensity[-1]
+
+        color_map = mpl.cm.bwr
+        norm = mpl.colors.Normalize(vmin=min_val, vmax=max_val)
+
+        color = []
+        for i in range(len(intensity)):
+            rgb = color_map(norm(intensity[i]), bytes = True)
+            color.append(rgb[:3])
+
+        #print(color)
+
+        #print(circles[0])
+        #print(color[0])
+
+        for i in range(len(intensity)):
+            draw.point(circles[i].tolist(), fill=color[i])
+            
+        #print(intensity)
+        #color_layer.save("color.png")
+        #so apparently I can only draw one thing at a time.
+        #This library is cursed
+        #bruh
+
+    sample.save("michelsons_contrast_raw.png")
+
+
